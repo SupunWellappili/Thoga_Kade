cdb672635ce74682ca72cd3e97e54488a28542ec
SupunWellappili <supunchandana1814@gmail.com>
2024-05-25 08:29:34 +0530

    Create Modal & Database

--- src/lk/gdse/ijse/pos/controller/CustomerFormController.java
+++ src/lk/gdse/ijse/pos/controller/CustomerFormController.java
@@ -1,4 +1,15 @@
 package lk.gdse.ijse.pos.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class CustomerFormController {
+    public TextField txtID;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtSalary;
+
+    public void saveCustomerOnAction(ActionEvent actionEvent) {
+
+    }
 }
