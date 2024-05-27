ebc0056071a4b1ba828cf75c5d09915a29e81001
SupunWellappili <supunchandana1814@gmail.com>
2024-05-27 12:53:48 +0530

    Create Item Form

--- src/lk/gdse/ijse/pos/controller/ItemFormController.java
+++ src/lk/gdse/ijse/pos/controller/ItemFormController.java
@@ -1,4 +1,33 @@
 package lk.gdse.ijse.pos.controller;
 
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class ItemFormController {
+
+
+    public TextField txtCode;
+    public TextField txtDescription;
+    public TextField txtUnitPrice;
+    public TextField txtQTYOnHand;
+    public TableView tblItem;
+    public TableColumn colCode;
+    public TableColumn colDescription;
+    public TableColumn colUnitPrice;
+    public TableColumn colQuntity;
+    public TableColumn colOption;
+    public Button btnSaveItem;
+
+    public void saveItemOnAction(ActionEvent actionEvent) {
+    }
+
+    public void newItemOnAction(ActionEvent actionEvent) {
+    }
+
+    public void backToHomeOnAction(ActionEvent actionEvent) {
+    }
 }
